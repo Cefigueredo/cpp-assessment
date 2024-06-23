99b9fc3f1770d04eeb8fe9463fcba69976eb3e6b
Carlos Figueredo <60144504+Cefigueredo@users.noreply.github.com>
2024-06-23 16:37:28 -0500

    Add exceptions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <set>
+class WrongFileException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "The file is not an XML or JSON file.";
+    }
+};
 
 class Employee {
 public:
@@ -11,7 +18,33 @@ public:
         int id, 
         const std::string& department, 
         int salary
-    ) : name(name), id(id), department(department), salary(salary) {}
+    ) : name{name}, id{id}, department{department}, salary{salary} {
+        if (name.empty()) {
+            throw std::invalid_argument("Name cannot be empty");
+        }
+        if (id <= 0) {
+            throw std::invalid_argument("ID must be a positive integer");
+        }
+        if (department.empty()) {
+            throw std::invalid_argument("Department cannot be empty");
+        }
+        if (salary <= 0) {
+            throw std::invalid_argument("Salary must be a positive integer");
+        }
+    }
+
+    std::string getName() const { return name; }
+    int getId() const { return id; }
+    std::string getDepartment() const { return department; }
+    int getSalary() const { return salary; }
+    void giveData() const {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "ID: " << id << std::endl;
+        std::cout << "Department: " << department << std::endl;
+        std::cout << "Salary: " << salary << std::endl;
+    }
+
+private:
     std::string name;
     int id;
     std::string department;
@@ -19,6 +52,7 @@ public:
 };
 
 std::vector<Employee> employees {};
+std::set<int> employeeIDs {};
 
 bool isXMLFile(const std::string &filePath) {
     std::ifstream file(filePath);
@@ -47,10 +81,7 @@ bool isJSONFile(const std::string &filePath) {
 
 void printEmployees() {
     for (const auto& employee : employees) {
-        std::cout << "Name: " << employee.name << std::endl;
-        std::cout << "ID: " << employee.id << std::endl;
-        std::cout << "Department: " << employee.department << std::endl;
-        std::cout << "Salary: " << employee.salary << std::endl;
+        employee.giveData();
         std::cout << std::endl;
     }
 }
@@ -58,49 +89,70 @@ void printEmployees() {
 void averageSalary() {
     int total = 0;
     for (const auto& employee : employees) {
-        total += employee.salary;
+        total += employee.getSalary();
     }
     double average = static_cast<double> (total) / employees.size();
     std::cout << "----- Average salary of the employees: ----"  << std::endl;
-    std::cout << "Average: " << average << std::endl;
+    std::cout << "Value: " << average << std::endl;
     std::cout << std::endl;
 }
 
 void getHighestPaidEmployee() {
     int maxSalary = 0;
-    Employee highestPaidEmployee("", 0, "", 0);
+    Employee highestPaidEmployee("Dummy", 1, "Dummy", 1);
     for (const auto& employee : employees) {
-        if (employee.salary > maxSalary) {
-            maxSalary = employee.salary;
+        if (employee.getSalary() > maxSalary) {
+            maxSalary = employee.getSalary();
             highestPaidEmployee = employee;
         }
     }
-    std::cout << "----- Highest paid employee: -----" << std::endl;
-    std::cout << "Name: " << highestPaidEmployee.name << std::endl;
-    std::cout << "ID: " << highestPaidEmployee.id << std::endl;
-    std::cout << "Department: " << highestPaidEmployee.department << std::endl;
-    std::cout << "Salary: " << highestPaidEmployee.salary << std::endl;
+    std::cout << "----- Highest paid employee: -----\n" << std::endl;
+    highestPaidEmployee.giveData();
     std::cout << std::endl;
 }
 
 void orderEmployeesByID() {
     std::sort(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
-        return a.id < b.id;
+        return a.getId() < b.getId();
     });
 }
 
 void handleOutput() {
+    std::cout << std::endl;
+    std::cout << "############################################\n";
+    std::cout << "####### Executing output handling... #######\n";
+    std::cout << "############################################\n";
+    std::cout << std::endl;
+    try {
+        if (employees.empty()) {
+            throw std::invalid_argument("No employees to handle output.");
+            return;
+        }
+    } catch (const std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+        return;
+    }
     averageSalary();
     getHighestPaidEmployee();
     orderEmployeesByID();
-    std::cout << "Employees ordered by ID:\n";
+    std::cout << "----- Employees ordered by ID: -----\n";
     printEmployees();
 }
 
 
 
 void addEmployee(const std::string& name, int id, const std::string& department, int salary) {
-    employees.push_back(Employee(name, id, department, salary));
+    try {
+        std::cout << "* Adding employee: " << name << std::endl;
+        if (employeeIDs.find(id) != employeeIDs.end()) {
+            throw std::invalid_argument("Duplicate ID found: " + std::to_string(id));
+        }
+        employees.push_back(Employee(name, id, department, salary));
+        employeeIDs.insert(id);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Exception adding employee: " << e.what() << std::endl;
+        return;
+    }
 }
 
 void parseXML(const std::string& filePath) {
@@ -111,10 +163,10 @@ void parseXML(const std::string& filePath) {
 
         if (root) {
             for (tinyxml2::XMLElement* employee = root->FirstChildElement("employee"); employee; employee = employee->NextSiblingElement("employee")) {
-                const char* name = employee->FirstChildElement("name")->GetText();
-                const char* id = employee->FirstChildElement("id")->GetText();
-                const char* department = employee->FirstChildElement("department")->GetText();
-                const char* salary = employee->FirstChildElement("salary")->GetText();
+                const char *name = employee->FirstChildElement("name")->GetText();
+                const char *id = employee->FirstChildElement("id")->GetText();
+                const char *department = employee->FirstChildElement("department")->GetText();
+                const char *salary = employee->FirstChildElement("salary")->GetText();
 
                 addEmployee(name, std::stoi(id), department, std::stoi(salary));
             }
@@ -123,7 +175,7 @@ void parseXML(const std::string& filePath) {
             std::cout << "No root element found.\n";
         }
     } else {
-        std::cout << "Failed to load XML file.\n";
+        throw std::runtime_error("Failed to load XML file.");
     }
 }
 
@@ -131,39 +183,46 @@ void parseJSON(const std::string& filePath) {
     std::ifstream file(filePath);
     nlohmann::json jsonData;
 
-    if (file >> jsonData) {
-        std::cout << "JSON file loaded successfully.\n";
+    try {
+        if (file >> jsonData) {
+            std::cout << "JSON file loaded successfully.\n";
+            std::cout << std::endl;
+            for (const auto& employee : jsonData["employees"]) {
+                std::string name = employee["name"];
+                int id = employee["id"];
+                std::string department = employee["department"];
+                int salary = employee["salary"];
 
-        for (const auto& employee : jsonData["employees"]) {
-            std::string name = employee["name"];
-            int id = employee["id"];
-            std::string department = employee["department"];
-            int salary = employee["salary"];
-
-            addEmployee(name, id, department, salary);
-            
+                addEmployee(name, id, department, salary);
+            }
+            handleOutput();
         }
-        handleOutput();
-    } else {
-        std::cout << "Failed to load JSON file.\n";
+    } catch (const nlohmann::json::exception& e) {
+        std::cerr << "Failed to load JSON file: " << e.what() << std::endl;
     }
 }
 
 int main() {
     std::cout << "Please insert the name of the file to read:" << std::endl;
     std::string filePath = "data/";
-    std::string fileName = "employees.json";
-    // std::cin >> fileName;
+    std::string fileName = "";
+    std::cin >> fileName;
     filePath += fileName;
     
-    if (isXMLFile(filePath)) {
-        std::cout << "The file is an XML file.\n";
-        parseXML(filePath);
-    } else if (isJSONFile(filePath)){
-        std::cout << "The file is a JSON file.\n";
-        parseJSON(filePath);
-    } else {
-        std::cerr << "The file is not an XML or JSON file.\n";
+    try {
+        if (isXMLFile(filePath)) {
+            parseXML(filePath);
+        } else if (isJSONFile(filePath)){
+            parseJSON(filePath);
+        } else {
+            throw WrongFileException();
+        }
+    } catch (const WrongFileException& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
     return 0;
 }
